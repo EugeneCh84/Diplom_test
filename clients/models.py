2e0d1568a2f6606a6cc02cd63c64cd647fc94743
EugeneCh_84 <eugene.cherepanov.84@gmail.com>
2024-02-10 16:27:29 +0500

    clients app

---
+++ clients/models.py
@@ -0,0 +1,30 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class Client(models.Model):
+    NEW = 'new'
+    OLD = 'old'
+    VIP = 'vip'
+    CHOICES_STATUS = (
+        (NEW, 'New'),
+        (OLD, 'Old'),
+        (VIP, 'VIP'),
+    )
+
+    company_name = models.CharField(max_length=255, blank=False, null=False)
+    phone = PhoneNumberField(max_length=15, blank=False, null=False, unique=True)
+    email = models.EmailField(max_length=255, blank=False, null=False)
+    address = models.CharField(max_length=255, blank=False, null=False)
+    zipcode = models.PositiveSmallIntegerField(blank=False, null=False)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, blank=False, null=False)
+    discount = models.PositiveSmallIntegerField(default=0, validators=([MaxValueValidator(50)]),
+                                                blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.company_name}'
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS += [
     'api',
     'common',
     'users',
+    'clients',
 ]
 
 MIDDLEWARE = [
